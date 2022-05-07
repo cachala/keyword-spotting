bc10aa7a1f4797c2e57d357a938a91cd590e5df0
ricae <104789086+ricae@users.noreply.github.com>
2022-05-07 18:22:21 +0800

    Update model.py

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 def get_model_instance_segmentation(num_classes):
   
     # load an instance segmentation model pre-trained on COCO
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
     # get number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
